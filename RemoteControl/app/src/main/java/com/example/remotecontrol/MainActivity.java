a71d9fa9e6ace6bf9ebb7c193cae2cf670039749
lvboqizw <925220146@qq.com>
2024-10-08 23:22:08 +0200

    Bluetooth connection

---
+++ RemoteControl/app/src/main/java/com/example/remotecontrol/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.remotecontrol;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+        implements BltManager.BluetoothCallback {
+
+    private static final String DEVICE_MAC_ADDRESS = "98:D3:31:F6:F6:AC";
+
+    private BltManager bluetoothManager;
+    private Button connectButton;
+    private boolean isConnected = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        connectButton = findViewById(R.id.btn_connect);
+        bluetoothManager = new BltManager(this);
+
+        connectButton.setOnClickListener(v -> {
+            if (!isConnected) {
+                bluetoothManager.connect(DEVICE_MAC_ADDRESS);
+            } else {
+                bluetoothManager.disconnect();
+            }
+        });
+
+    }
+
+    public void onConnectionStatusChanged(boolean isConnected) {
+        this.isConnected = isConnected;
+        runOnUiThread(() -> {
+            if (isConnected) {
+                connectButton.setBackgroundColor(getResources()
+                        .getColor(R.color.green));
+                connectButton.setText(getResources()
+                        .getText(R.string.connected));
+                Toast.makeText(MainActivity.this,
+                        "Connected",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                connectButton.setBackgroundColor(getResources()
+                        .getColor(R.color.warning_red));
+                connectButton.setText(getResources()
+                        .getText(R.string.disconnected));
+                Toast.makeText(MainActivity.this,
+                        "Disconnected",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onMessageReceived(String message) {
+        runOnUiThread(() -> Toast.makeText(
+                this,
+                "Message: " + message,
+                Toast.LENGTH_SHORT).show());
+    }
+
+}
